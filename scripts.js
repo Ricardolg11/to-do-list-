b26a8bdd1a5b3f9941a11d15417a6b9d8d049438
ricardo lg <solop162@gmail.com>
2023-07-19 13:47:14 -0300

    new commit

---
+++ scripts.js
@@ -0,0 +1,59 @@
+const button = document.querySelector('.button-add-task')
+const input = document.querySelector('.input-task')
+const listcomplet = document.querySelector('.list-tasks')
+let listaDeItens = []
+
+
+function addNovaTarefa( ) {
+    listaDeItens.push({
+        tarefa: input.value,
+        concluida: false
+    })
+
+    input.value = ''
+
+    mostraTarefa()
+}
+
+
+function mostraTarefa(){
+    let novali = ''
+
+    listaDeItens.forEach( (item, index) => {
+
+        novali = novali + `
+        <li class="task ${item.concluida && "done"}">
+        <img src="./img/checked.png" alt="chek" onclick="concluirTarefa(${index})" >
+        <p class="text">${item.tarefa}</p>
+        <img src="./img/trash.png" alt="trash" onclick="deletarItem(${index})" >
+        </li>
+        `
+      
+    })
+
+    listcomplet.innerHTML = novali;
+
+    localStorage.setItem('lista', JSON.stringify(listaDeItens))
+}
+
+function concluirTarefa(index){
+    listaDeItens[index].concluida = !listaDeItens[index].concluida 
+    mostraTarefa()
+}
+
+function deletarItem(index){
+    listaDeItens.splice(index, 1)
+    mostraTarefa()
+}
+
+function recarregarTarefas(){
+    const tarefaslocalstorage = localStorage.getItem('lista')
+    if(tarefaslocalstorage){
+        listaDeItens = JSON.parse(tarefaslocalstorage);
+    }
+
+    mostraTarefa()
+}
+
+recarregarTarefas()
+button.addEventListener('click', addNovaTarefa)
